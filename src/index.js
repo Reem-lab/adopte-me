0d40602bad5fad1951de2742ac2c04abc1858d52
Reem <janinareem@gmail.com>
2022-02-21 15:50:23 +0200

    Add the app page to the index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+     <div id="modal"></div>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
